8eac5bec744b57efce7967e9a9322d85bcc54f5e
fadhilahwanda@gmail.com <fadhillahwanda23@gmail.com>
2021-09-03 16:20:27 +0700

    migration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-  public function me() {
-    return ['NIS' => 3103119044,
-        'name' => 'Fadhilah Wanda Almira',
-        'gender' => 'Perempuan',
-        'phone' => '085293809149',
-        'class' => 'XII RPL 2'];
-  }
+    public function me()
+    {
+        return
+        [
+            "NIS" => 3103119065,
+            "Nama" => "Fadhilah",
+            "gender" => "Perempuan",
+            "phone" => 085293809149,
+            "class" => "XII RPL 2"
+        ];
+    }
 }
